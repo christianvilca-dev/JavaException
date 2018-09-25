b72162b4d8c34c64c684c2fcfd00ba87d2b60d10
Christian <Christian@Tom>
2018-09-24 19:00:58 -0500

    Excepcion

---
+++ JavaExceptionNetbeans/src/com/christianvilca/Excepciones/Excepcion1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.christianvilca.Excepciones;
+
+/**
+ *
+ * @author Christian
+ */
+public class Excepcion1 {
+//    try{} 
+//    catch{}   ->  Parametro que identifica la excepcion a manejar
+//                  Se realiza algun proceso
+//                  Opcional si no esta "finaly{}"
+//                  Pueden haber varios catch
+//                  Entre paramentros puede estar la condicional or(|)
+//    finaly{}  ->  Va despues del ultimo "catch{}"
+//                  Se ejecuta siempre (exista o no excepcion)
+//    
+//    throws    ->  "eleva" una excepcion a un metodo de un nivel superior
+//                  (envia la excepcion para que sea manejada por el metodo
+//                  que solicito un proceso especifico)
+//    throw     ->  Lanza una excepcion para que sea manejada en un bloque
+//                  try catch
+}

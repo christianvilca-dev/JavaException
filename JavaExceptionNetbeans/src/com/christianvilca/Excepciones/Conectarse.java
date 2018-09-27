493d3569edeee01776c888b5c46eda464f9ac111
Christian <Christian@Tom>
2018-09-27 18:52:23 -0500

    Cap3.3 Clausula Finally

--- JavaExceptionNetbeans/src/com/christianvilca/Excepciones/Conectarse.java
+++ JavaExceptionNetbeans/src/com/christianvilca/Excepciones/Conectarse.java
@@ -19,16 +19,19 @@ public class Conectarse {
     }
     
     public void conectar() {
+        Connection conn= null;
+        Statement sentencia = null;
+        ResultSet resultados = null;
         try {
-            Class.forName("org.una.Driver"); // https://jdbc.postgresql.org/
+            Class.forName("org.postgresql.Driver"); // https://jdbc.postgresql.org/
             String url="jdbc:postgresql://localhost:5432/test"; //5432 
             Properties props = new Properties();
 //            props.setProperty("user","postgres");
             props.setProperty("user","ursprueba");
-            props.setProperty("password","abcd.1234");
-            Connection conn= DriverManager.getConnection(url, props);
-            Statement sentencia = conn.createStatement();
-            ResultSet resultados = sentencia.executeQuery("select * from usuario");
+            props.setProperty("password","abcd.12345");
+            conn= DriverManager.getConnection(url, props);
+            sentencia = conn.createStatement();
+            resultados = sentencia.executeQuery("select * from usuario");
             while(resultados.next()){
                 System.out.print("id: "+resultados.getString(1));
                 System.out.print("\t");
@@ -36,6 +39,10 @@ public class Conectarse {
             }
         } catch (ClassNotFoundException | SQLException myException) {
             System.out.println("Excepcion encontrada: " + myException.getMessage());
+        } finally { // recomendada para liberar recursos    
+            if (resultados != null) try { resultados.close(); } catch (SQLException sqle) {}
+            if (sentencia != null) try { sentencia.close(); } catch (SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch (SQLException sqle) {}
         }
     }
     

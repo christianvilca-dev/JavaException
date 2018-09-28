3777bab9fc70cc7a1db312c6c2c360183be36c5a
Christian <Christian@Tom>
2018-09-27 19:43:20 -0500

    Cap5 Excepciones propias (Excepcion no verificada- RESULTADO)

--- JavaExceptionNetbeans/src/com/christianvilca/Excepciones/Excepcion5Division.java
+++ JavaExceptionNetbeans/src/com/christianvilca/Excepciones/Excepcion5Division.java
@@ -28,4 +28,13 @@ public class Excepcion5Division {
     public static void main(String[] args) {
         new Excepcion5Division().dividir();
     }
+    ////////////////////////////////////////////////
+//    run:
+//Exception in thread "main" com.christianvilca.Excepciones.Excepcion5: No voy a No voy a dividir por uno.
+//	at com.christianvilca.Excepciones.Excepcion5Division.divide(Excepcion5Division.java:22)
+//	at com.christianvilca.Excepciones.Excepcion5Division.dividir(Excepcion5Division.java:16)
+//	at com.christianvilca.Excepciones.Excepcion5Division.main(Excepcion5Division.java:29)
+//C:\Users\Christian\AppData\Local\NetBeans\Cache\8.2\executor-snippets\run.xml:53: Java returned: 1
+//BUILD FAILED (total time: 0 seconds)
+        ////////////////////////////////////////////////
 }
